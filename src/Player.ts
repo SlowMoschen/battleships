48d2ba8c388191a27b133ffe5b504016c249549b
phil@home <philipp.millner@outlook.com>
2024-07-05 01:26:30 +0200

    refactor: Refactor Player class to implement real and bot player classes

--- src/Player.ts
+++ src/Player.ts
@@ -1,49 +1,124 @@
 import { ships } from "../config";
 import { Gameboard } from "./Gameboard";
 import { Ship } from "./Ship";
+import { CellStates } from "./types/CellStates";
 import { Point } from "./types/Point";
 
 type ShipNames = keyof typeof ships;
 
-export class Player {
-    public isBot: boolean;
-    public name: string;
-    public gameBoard: Gameboard
-    private missedShots: number = 0;
+abstract class Player {
+  abstract isBot: boolean;
+  abstract name: string;
+  public gameBoard: Gameboard = new Gameboard();
 
-    constructor(name?: string) {
-        this.isBot = name ? false : true;
-        this.name = name || "Bot";
-        this.gameBoard = new Gameboard();
+  abstract makeMove(enemyBoard: Gameboard, point?: Point): void;
 
-        if (this.isBot) {
-            this.gameBoard.placeShipsRandomly();
-        }
+  isValidAttack(point: Point): boolean {
+    const isWithinBounds = point.x >= 0 && point.x < Gameboard.BOARD_SIZE && point.y >= 0 && point.y < Gameboard.BOARD_SIZE;
+    if (!isWithinBounds) return false;
+
+    const hasNotBeenAttacked = this.gameBoard.board[point.x][point.y] === CellStates.EMPTY;
+    const hasNotBeenMissed = this.gameBoard.board[point.x][point.y] !== CellStates.MISS;
+    const hasNotBeenHit = this.gameBoard.board[point.x][point.y] !== CellStates.HIT;
+
+    return isWithinBounds && hasNotBeenAttacked && hasNotBeenMissed && hasNotBeenHit;
+  }
+
+  receiveAttack(point: Point): void {
+    this.gameBoard.receiveAttack(point);
+  }
+}
+
+// MARK: RealPlayer
+export class RealPlayer extends Player {
+  public isBot: boolean;
+  public name: string;
+  public gameBoard: Gameboard = new Gameboard();
+
+  constructor(name: string) {
+    super();
+    this.isBot = false;
+    this.name = name;
+  }
+
+  public makeMove(enemyBoard: Gameboard, point: Point): void {
+    if (this.isValidAttack(point)) {
+      enemyBoard.receiveAttack(point);
+    }
+  }
+
+  public placeShip(shipName: ShipNames, orientation: string, start: Point): void {
+    let coordinates: Point[] = [];
+    const ship = ships[shipName];
+
+    for (let i = 0; i < ship.size; i++) {
+      if (orientation === "horizontal") {
+        coordinates.push({ x: start.x + i, y: start.y });
+      } else {
+        coordinates.push({ x: start.x, y: start.y + i });
+      }
     }
 
-    public makeMove(point: Point): void {
-        this.gameBoard.receiveAttack(point);
+    const shipInstance = new Ship(ship.name, ship.size, coordinates);
+    this.gameBoard.placeShip(shipInstance, coordinates);
+  }
+}
+
+// MARK: BotPlayer
+export class BotPlayer extends Player {
+  public isBot: boolean;
+  public name: string;
+  public gameBoard: Gameboard = new Gameboard();
+  private lastMove: {
+    coordinates: Point;
+    hit: boolean;
+  } | null = null;
+
+  constructor() {
+    super();
+    this.isBot = true;
+    this.name = "Bot";
+    this.gameBoard.placeShipsRandomly();
+  }
+
+  public makeMove(enemyBoard: Gameboard, point?: Point): void {
+    if (!point) {
+        point = this.lastMove && this.lastMove.hit ? this.getAdjacentPoint() : this.getRandomPoint();
     }
 
-    public placeShip(shipName: ShipNames, orientation: string, start: Point): void {
-        let coordinates: Point[] = [];
-        const ship = ships[shipName];
+    enemyBoard.receiveAttack(point);
+    this.lastMove = {
+      coordinates: point,
+      hit: enemyBoard.board[point.x][point.y] === CellStates.HIT,
+    };
+}
 
-        for (let i = 0; i < ship.size; i++) {
-            if (orientation === "horizontal") {
-                coordinates.push({ x: start.x + i, y: start.y });
-            } else {
-                coordinates.push({ x: start.x, y: start.y + i });
-            }
-        }
+  private getRandomPoint(): Point {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
 
-        const shipInstance = new Ship(ship.name, ship.size, coordinates);
-        this.gameBoard.placeShip(shipInstance, coordinates);
-    }
+    return { x, y };
+  }
 
+  private getAdjacentPoint(): Point {
+    let point: Point;
+    const directions = [
+        { x: -1, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: -1 },
+        { x: 0, y: 1 },
+        ];
+    const { x, y } = this.lastMove!.coordinates;
 
-    // MARK: GETTERS
-    get missedShotsCount(): number {
-        return this.missedShots;
-    }
+    do {
+        const direction = directions[Math.floor(Math.random() * directions.length)];
+        point = { x: x + direction.x, y: y + direction.y };
+    } while (!this.isValidAttack(point));
+
+    return point;
+  }
+
+  get lastMoveCoordinates(): Point | null {
+    return this.lastMove?.coordinates || null;
+  }
 }
--- src/__test__/Player.spec.ts
+++ src/__test__/Player.spec.ts
@@ -1,51 +1,105 @@
 import { beforeEach, describe, expect, test } from "vitest";
-import { Player } from "../Player";
+import { RealPlayer, BotPlayer } from "../Player";
 import { CellStates } from "../types/CellStates";
 
+// MARK: Player initialization
 describe("Player initialization", () => {
-    test("should create a bot player if no name is provided", () => {
-        const player = new Player();
-        expect(player.isBot).toBe(true);
-        expect(player.name).toBe("Bot");
-    });
+  test("should create a bot player if no name is provided", () => {
+    const player = new BotPlayer();
+    expect(player.isBot).toBe(true);
+    expect(player.name).toBe("Bot");
+  });
 
-    test("should create a human player if a name is provided", () => {
-        const player = new Player("Alice");
-        expect(player.isBot).toBe(false);
-        expect(player.name).toBe("Alice");
-    });
+  test("should create a human player if a name is provided", () => {
+    const player = new RealPlayer("Alice");
+    expect(player.isBot).toBe(false);
+    expect(player.name).toBe("Alice");
+  });
 
-    test("should have a gameBoard", () => {
-        const player = new Player();
-        expect(player.gameBoard).not.toBe(undefined);
-    });
+  test("should have a gameBoard", () => {
+    const player = new RealPlayer("Alice");
+    expect(player.gameBoard).not.toBe(undefined);
+  });
 
-    test("should place ships randomly if player is a bot", () => {
-        const player = new Player();
-        expect(player.gameBoard.ships.length).toBe(5);
-    });
+  test("should place ships randomly if player is a bot", () => {
+    const player = new BotPlayer();
+    expect(player.gameBoard.ships.length).toBe(5);
+  });
 });
 
+// MARK: Player actions
 describe("Player actions", () => {
-    let player: Player;
+  let player: RealPlayer;
+  let bot: BotPlayer;
 
-    beforeEach(() => {
-        player = new Player('Alice');
-    });
+  beforeEach(() => {
+    player = new RealPlayer("Alice");
+    bot = new BotPlayer();
+  });
 
-    test("should place a ship on the gameboard", () => {
-        player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
-        expect(player.gameBoard.ships.length).toBe(1);
-    });
+  test("isValidAttack Tests", () => {
+    expect(player.isValidAttack({ x: 0, y: 0 })).toBe(true);
+    expect(player.isValidAttack({ x: 10, y: 10 })).toBe(false);
+  });
 
-    test("shouldn't place a ship if the coordinates are invalid", () => {
-        player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
-        player.placeShip("battleship", "horizontal", { x: 0, y: 0 });
-        expect(player.gameBoard.ships.length).toBe(1);
-    });
+  test("should make a move", () => {
+    player.makeMove(bot.gameBoard, { x: 0, y: 0 });
+    expect(bot.gameBoard.board[0][0]).not.toBe(CellStates.EMPTY);
+  });
 
-    test("should make a move on the gameboard", () => {
-        player.makeMove({ x: 0, y: 0 });
-        expect(player.gameBoard.board[0][0]).toBe(CellStates.MISS);
-    });
+  test("should receive an attack", () => {
+    player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
+    bot.makeMove(player.gameBoard, { x: 0, y: 0 });
+    expect(player.gameBoard.board[0][0]).toBe(CellStates.HIT);
+  });
+});
+
+// MARK: RealPlayer actions
+describe("RealPlayer actions", () => {
+  let player: RealPlayer;
+
+  beforeEach(() => {
+    player = new RealPlayer("Alice");
+  });
+
+  test("should place a ship on the gameboard", () => {
+    player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
+    expect(player.gameBoard.ships.length).toBe(1);
+  });
+
+  test("shouldn't place a ship if the coordinates are invalid", () => {
+    player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
+    player.placeShip("battleship", "horizontal", { x: 0, y: 0 });
+    expect(player.gameBoard.ships.length).toBe(1);
+  });
+
+  test("should receive an attack", () => {
+    player.receiveAttack({ x: 0, y: 0 });
+    expect(player.gameBoard.board[0][0]).toBe(CellStates.MISS);
+  });
+});
+
+// MARK: BotPlayer actions
+describe("BotPlayer actions", () => {
+  let bot: BotPlayer;
+  let player: RealPlayer;
+
+  beforeEach(() => {
+    bot = new BotPlayer();
+    player = new RealPlayer("Alice");
+  });
+
+  test("should make a random move", () => {
+    bot.makeMove(player.gameBoard);
+    const attackedCell = bot.lastMoveCoordinates!;
+    expect(player.gameBoard.board[attackedCell.x][attackedCell.y]).not.toBe(CellStates.EMPTY);
+  });
+
+  test("should make an adjacent move if the last move was a hit", () => {
+    player.placeShip("carrier", "horizontal", { x: 0, y: 0 });
+    bot.makeMove(player.gameBoard, { x: 0, y: 0 });
+    bot.makeMove(player.gameBoard);
+    const attackedCell = bot.lastMoveCoordinates!;
+    expect(player.gameBoard.board[attackedCell.x][attackedCell.y]).not.toBe(CellStates.EMPTY);
+  });
 });
